78aa2213a1cc89892888831dd9c2ec49ffa511cf
iradbouzidi <b.irad1994@gmail.com>
2023-12-20 18:51:55 +0100

    clean

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -13,15 +13,11 @@ interface ListItemProps {
   href: string;
 }
 
-const ListItem: React.FC<ListItemProps> = ({
-  image,
-  name,
-  href,
-}) => {
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   const router = useRouter();
   const authModal = useAuthModal();
   const { user } = useUser();
-  
+
   const onClick = () => {
     if (!user) {
       return authModal.onOpen();
@@ -30,7 +26,7 @@ const ListItem: React.FC<ListItemProps> = ({
     router.push(href);
   };
 
-  return ( 
+  return (
     <button
       onClick={onClick}
       className="
@@ -49,17 +45,10 @@ const ListItem: React.FC<ListItemProps> = ({
       "
     >
       <div className="relative min-h-[64px] min-w-[64px]">
-        <Image
-          className="object-cover"
-          src={image}
-          fill
-          alt="Image"
-        />
+        <Image className="object-cover" src={image} fill alt="Image" />
       </div>
-      <p className="font-medium truncate py-5">
-        {name}
-      </p>
-      <div 
+      <p className="font-medium truncate py-5">{name}</p>
+      <div
         className="
           absolute 
           transition 
@@ -79,7 +68,7 @@ const ListItem: React.FC<ListItemProps> = ({
         <FaPlay className="text-black" />
       </div>
     </button>
-   );
-}
- 
+  );
+};
+
 export default ListItem;
